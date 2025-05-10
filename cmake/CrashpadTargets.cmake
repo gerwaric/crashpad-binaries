4e860663c451287721cb9c35b478b923b5e489c2
GERWARIC <gerwaric@gmail.com>
2025-05-10 08:40:23 -0700

    First macOS binaries commit

--- cmake/CrashpadTargets.cmake
+++ cmake/CrashpadTargets.cmake
@@ -4,15 +4,41 @@ set(_crashpad_base "${CMAKE_CURRENT_LIST_DIR}/..")
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(_crashpad_platform "Linux")
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    set(_crashpad_platform "Linux") #"macOS")
+    set(_crashpad_platform "macOS")
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
     set(_crashpad_platform "Windows")
 else()
-    message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME}")
+    message(FATAL_ERROR "Crashpad: unsupported platform: ${CMAKE_SYSTEM_NAME}")
 endif()
 
-add_library(crashpad-binaries STATIC IMPORTED)
-set_target_properties(crashpad-binaries PROPERTIES
-    IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libcrashpad.a"
+add_library(Crashpad INTERFACE)
+target_link_libraries(Crashpad INTERFACE
+    Crashpad::Client
+    Crashpad::Common
+    Crashpad::Util
+    MiniChromium::Base
+)
+
+add_library(Crashpad::Client STATIC IMPORTED)
+set_target_properties(Crashpad::Client PROPERTIES
+    IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libclient.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
+)
+
+add_library(Crashpad::Common STATIC IMPORTED)
+set_target_properties(Crashpad::Common PROPERTIES
+    IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libcommon.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
+)
+
+add_library(Crashpad::Util STATIC IMPORTED)
+set_target_properties(Crashpad::Util PROPERTIES
+    IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libutil.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
+)
+
+add_library(MiniChromium::Base STATIC IMPORTED)
+set_target_properties(MiniChromium::Base PROPERTIES
+    IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libbase.a"
     INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
 )
---
+++ scripts/build-macos.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "$(uname -s)" != "Darwin" ]]; then
+    echo "This script must be run on macOS (Darwin)." >&2
+    exit 1
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR="${SCRIPT_DIR}/.."
+BUILD_DIR="${ROOT_DIR}/build"
+OUT_DIR="${BUILD_DIR}/out"
+OBJ_DIR="${BUILD_DIR}/obj"
+
+DEPOT_TOOLS_URL="https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+DEPOT_TOOLS_DIR="${BUILD_DIR}/depot_tools"
+
+INCLUDE_DIR="${ROOT_DIR}/include/"
+LIB_DIR="${ROOT_DIR}/lib/macOS"
+
+# Step 2: Fetch Crashpad source
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir -p "${BUILD_DIR}"
+fi
+
+# Step 1: Set up depot_tools
+if [ ! -d "${DEPOT_TOOLS_DIR}" ]; then
+    echo "Installing depot_tools..."
+    mkdir -p "${DEPOT_TOOLS_DIR}"
+    git clone "${DEPOT_TOOLS_URL}" "${DEPOT_TOOLS_DIR}"
+fi
+export PATH="${DEPOT_TOOLS_DIR}${PATH+:${PATH}}"
+
+# Step 2: Fetch Crashpad source
+if [ ! -d "${BUILD_DIR}/crashpad" ]; then
+    echo "Fetching Crashpad source..."
+    cd "${BUILD_DIR}"
+    fetch crashpad
+fi
+
+echo "Building Crashpad..."
+cd "${BUILD_DIR}/crashpad"
+gn gen "${OUT_DIR}" --args="target_cpu=\"mac_universal\""
+ninja -C "${OUT_DIR}"
+
+# Step 4: Build the combined output library
+echo "Installing the library..."
+if [ ! -d "${LIB_DIR}" ]; then
+    mkdir -p "${LIB_DIR}"
+fi
+cp "${OUT_DIR}/obj/client/libclient.a" "${LIB_DIR}"
+cp "${OUT_DIR}/obj/client/libcommon.a" "${LIB_DIR}"
+cp "${OUT_DIR}/obj/util/libutil.a" "${LIB_DIR}"
+cp "${OUT_DIR}/obj/third_party/mini_chromium/mini_chromium/base/libbase.a" "${LIB_DIR}"
+
+# Step 3: Copy headers
+echo "Installing client headers..."
+rsync -a --delete --include='*/' --include='*.h' --include='*.hpp' --exclude='*' \
+    "${BUILD_DIR}/crashpad/client/" \
+    "${INCLUDE_DIR}/crashpad/client/"
+echo "Installing util headers..."
+rsync -a --delete --include='*/' --include='*.h' --include='*.hpp' --exclude='*' \
+    "${BUILD_DIR}/crashpad/util/" \
+    "${INCLUDE_DIR}/crashpad/util/"
+echo "Installing mini_chromium headers..."
+rsync -a --delete --include='*/' --include='*.h' --include='*.hpp' --exclude='*' \
+    "${BUILD_DIR}/crashpad/third_party/mini_chromium/mini_chromium/base" \
+    "${INCLUDE_DIR}/mini_chromium/base"

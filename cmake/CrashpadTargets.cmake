6a3fe527a92a035fe3d7751d31a288b8931c26cb
GERWARIC <gerwaric@gmail.com>
2025-05-10 08:52:15 -0700

    Name changes

--- cmake/CrashpadTargets.cmake
+++ cmake/CrashpadTargets.cmake
@@ -11,34 +11,34 @@ else()
     message(FATAL_ERROR "Crashpad: unsupported platform: ${CMAKE_SYSTEM_NAME}")
 endif()
 
-add_library(Crashpad INTERFACE)
-target_link_libraries(Crashpad INTERFACE
-    Crashpad::Client
-    Crashpad::Common
-    Crashpad::Util
-    MiniChromium::Base
+add_library(crashpad INTERFACE)
+target_link_libraries(crashpad INTERFACE
+    crashpad::client
+    crashpad::common
+    crashpad::util
+    mini_chromium::base
 )
 
-add_library(Crashpad::Client STATIC IMPORTED)
-set_target_properties(Crashpad::Client PROPERTIES
+add_library(crashpad::client STATIC IMPORTED)
+set_target_properties(crashpad::client PROPERTIES
     IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libclient.a"
     INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
 )
 
-add_library(Crashpad::Common STATIC IMPORTED)
-set_target_properties(Crashpad::Common PROPERTIES
+add_library(crashpad::common STATIC IMPORTED)
+set_target_properties(crashpad::common PROPERTIES
     IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libcommon.a"
     INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
 )
 
-add_library(Crashpad::Util STATIC IMPORTED)
-set_target_properties(Crashpad::Util PROPERTIES
+add_library(crashpad::util STATIC IMPORTED)
+set_target_properties(crashpad::util PROPERTIES
     IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libutil.a"
     INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
 )
 
-add_library(MiniChromium::Base STATIC IMPORTED)
-set_target_properties(MiniChromium::Base PROPERTIES
+add_library(mini_chromium::base STATIC IMPORTED)
+set_target_properties(mini_chromium::base PROPERTIES
     IMPORTED_LOCATION "${_crashpad_base}/lib/${_crashpad_platform}/libbase.a"
     INTERFACE_INCLUDE_DIRECTORIES "${_crashpad_base}/include"
 )
